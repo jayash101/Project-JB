50ebae7d2d1100c4cd6170bc35a842f786cc7181
jayash101 <jayashbasnetit@gmail.com>
2024-03-08 11:42:28 +0545

    Nav scroll added

--- Scrolling Navbar/script.js
+++ Scrolling Navbar/script.js
@@ -1,3 +1,17 @@
 // Current Year
 const currentDate = document.querySelector(".currentDate");
 currentDate.textContent = new Date().getFullYear();
+
+// Navbar
+const navBar = document.querySelector(".navBar");
+window.addEventListener("scroll", () => {
+    // Responsive JS
+    if(window.innerWidth > 768){
+        if (window.pageYOffset > 0) {
+            navBar.classList.add("navBarScroll");
+        }
+        else {
+            navBar.classList.remove("navBarScroll");
+        }
+    }
+})
